a587e059700a2dd6260c54e64f1430de3d8045a8
jaiminpatel98 <jaiminpatel98@gmail.com>
2017-01-15 18:40:00 -0500

    finsihed my functions

--- A1/A1Q4.java
+++ A1/A1Q4.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
+
 /**
  * The class <b>A1Q4</b> is an implementation of the
  * ``Old Maid'' card game, based on the Python implementation
@@ -12,6 +13,131 @@ import java.util.Random;
  */
 
 public class A1Q4{
+
+	/** 
+     * Prints the strings contained in arrayOfStrings
+     * 
+     *   @param arrayOfStrings the array of Strings
+     *   @param currentSize the number of strings in the arrayOfStrings,
+     *			stored from arrayOfStrings[0] to arrayOfStrings[currentSize-1] 
+     */
+
+	public static void printArray(String[] arrayOfStrings, int currentSize){
+		if( arrayOfStrings == null || currentSize > arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.printArray: wrong call");
+			return ;
+		}
+		for(int i = 0; i < currentSize-1; i++){
+			System.out.print(arrayOfStrings[i] + ", ");
+		}
+		System.out.println(arrayOfStrings[currentSize-1]);
+	}
+
+	/** 
+     * Does a lexicographic sorting of arrayOfStrings. 
+     * It simply relies on the sort(Object[]) method
+	 * of java.util.Arrays. 
+	 * Note that it sorts in lexicographic order, so 
+	 * "10" is before "2", "A" is before "Q" etc. 
+	 * This is not important for this game, but wouldn't 
+	 * be suitable in some other cases
+     * 
+     *   @param arrayOfStrings the array of Strings
+     *   @param currentSize the number of strings in the arrayOfStrings,
+     *			stored from arrayOfStrings[0] to arrayOfStrings[currentSize-1] 
+     */
+	public static void sortArray(String[] arrayOfStrings, int currentSize){
+		if( arrayOfStrings == null || currentSize > arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.sortArray: wrong call");
+			return ;
+		}
+		java.util.Arrays.sort(arrayOfStrings, 0, currentSize );
+	}
+
+
+
+	/** 
+     * Randomly shuffles arrayOfStrings. 
+     * 
+     *   @param arrayOfStrings the array of Strings
+     *   @param currentSize the number of strings in the arrayOfStrings,
+     *			stored from arrayOfStrings[0] to arrayOfStrings[currentSize-1] 
+     */
+	public static void shuffleArray(String[] arrayOfStrings, int currentSize){
+		if( arrayOfStrings == null || currentSize > arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.shuffleArray: wrong call");
+			return ;
+		}
+		
+		java.util.Random generator = new java.util.Random();
+
+		for(int i = currentSize-1 ; i > 1 ; i--){
+			swapItems(arrayOfStrings, i,generator.nextInt(i-1));
+		}
+	}
+
+	private static void swapItems(String[] arrayOfStrings, int i, int j){
+		String intermediate = arrayOfStrings[i];
+		arrayOfStrings[i]=arrayOfStrings[j];
+		arrayOfStrings[j]=intermediate;
+	}
+
+
+	/** 
+     * Removes the string at  arrayOfStrings[itemToRemove] and
+     * 
+     *   @param arrayOfStrings the array of Strings
+     *   @param currentSize the number of strings in the arrayOfStrings,
+     *			stored from arrayOfStrings[0] to arrayOfStrings[currentSize-1] 
+     *   @param itemToRemove index of the item to remove from arrayOfStrings
+     *   @return the new size of the modified arrayOfStrings
+     */
+
+	public static int removeItemByIndex(String[] arrayOfStrings, int currentSize, int itemToRemove){
+
+		if( arrayOfStrings == null || currentSize > arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.removeItemByIndex: wrong call");
+			return currentSize;
+		}
+		if( itemToRemove < 0 || itemToRemove >= currentSize ) {
+			System.out.println("ArrayStringsTools.removeItem: item " 
+				+ itemToRemove + " out of bounds. Array Unchanged.");
+			return currentSize;
+		}
+
+		int i;
+		for( i = itemToRemove; i < currentSize-1; i++){
+			arrayOfStrings[i] = arrayOfStrings[i+1];
+		}
+		arrayOfStrings[i]= null;
+		return currentSize-1;
+	}
+
+	/** 
+     * Appends itemToAdd at the end of arrayOfStrings 
+     * 
+     * 
+     *   @param arrayOfStrings the array of Strings
+     *   @param currentSize the number of strings in the arrayOfStrings,
+     *			stored from arrayOfStrings[0] to arrayOfStrings[currentSize-1] 
+     *   @param itemToAdd the String to add to  arrayOfStrings
+     *   @return the new size of the modified arrayOfStrings
+     */
+
+ 	public static int appendItem(String[] arrayOfStrings, int currentSize, String itemToAdd){
+
+		if( arrayOfStrings == null || currentSize > arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.appendItem: wrong call");
+			return currentSize;
+		}
+
+		if( currentSize == arrayOfStrings.length) {
+			System.out.println("ArrayStringsTools.appendItem: array full. Array Unchanged.");
+			return currentSize;
+		}
+		arrayOfStrings[currentSize++]=itemToAdd;
+		return currentSize;
+	} 	
 
 	/**
 	* Array used to store the full deck of cards,
@@ -90,9 +216,14 @@ public class A1Q4{
 	*  into playerDeck and computerDeck, starting with playerDeck
 	*/
 	private static void dealCards(){
-
-
-// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+		for (int i=0; i<deck.length; i++) {
+			if (i%2==0) {
+				appendItem(playerDeck, sizePlayerDeck, deck[i]);
+			}
+			else {
+				appendItem(computerDeck, sizeComputerDeck, deck[i]);
+			}
+		}
 
 	}
 
@@ -109,7 +240,7 @@ public class A1Q4{
 	private static int removePairs(String[] deckOfCards, int currentSize){
 	
 // REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
-
+		return(2);
 
 	}
 
@@ -120,8 +251,30 @@ public class A1Q4{
 	*  @return the valid input.
 	*/
 	private static int getValidInput(){
-
-// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+		System.out.println("Please a number between 1 and " + sizeComputerDeck);
+		int userin = sc.nextInt();
+		boolean flag = true;
+		while (true) {
+			if ((userin>=1) && (userin<=sizeComputerDeck)) {
+				if (userin == 1) {
+					System.out.println("You chose my "+ userin + "st card");
+				}
+				else if (userin == 2) {
+					System.out.println("You chose my " + userin + "nd card");
+				}
+				else if (userin == 3) {
+					System.out.println("You chose my " + userin + "rd card");
+				}
+				else {
+					System.out.println("You chose my " + userin + "th card");
+				}
+				return (userin);
+			}
+			else {
+				System.out.println("Invalid answer. Give me an integer between 1 and " + sizeComputerDeck + ": ");
+				flag = true;
+			}
+		}
 	
 	}
 
